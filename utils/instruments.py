03e9e7e6cb880a62200b0673b67a44287203244f
simion.gorgos@gmail.com <Random123!@#>
2020-01-12 22:20:23 +0200

    Remove unused function is_city

--- utils/instruments.py
+++ utils/instruments.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 import re
 
-
 def get_time_from_seconds(seconds):
     return datetime.fromtimestamp(seconds).strftime("%I:%M %p")
 
@@ -10,11 +9,6 @@ def deg_to_direction(deg):
     val = int((deg / 22.5) + .5)
     arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"]
     return arr[(val % 16)]
-
-
-def is_city(city):
-    return lambda: re.compile(r"[a-zA-Z]+(?:[ '-][a-zA-Z]+)*").fullmatch(city)
-
 
 
 
